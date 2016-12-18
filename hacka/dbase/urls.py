413ffe2c251a17ef37091e4a5c3bab0aaeab0354
lukir <radluki@gmail.com>
2016-12-18 02:17:46 +0100

    get json, send localization, TODO register

--- hacka/dbase/urls.py
+++ hacka/dbase/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^send/(?P<login>[a-zA-Z]+)/(?P<password>[a-zA-Z]+)$', views.send, name='send'),
+    url(r'^sendloc/(?P<login>[a-zA-Z]+)/(?P<password>[a-zA-Z]+)/(?P<longitude>[0-9]+)/(?P<latitude>[0-9]+)/$', views.sendloc, name='sendloc'),
+    url(r'^register/(?P<login>[a-zA-Z]+)/(?P<password>[a-zA-Z]+)$',views.register,name='regiser'),
 ]
--- hacka/dbase/views.py
+++ hacka/dbase/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core import serializers
+import json
 
 # Create your views here.
 
@@ -19,7 +21,32 @@ def vote(request, question_id):
     return HttpResponse(question)
 
 def send(request, login, password):
+    try:
+        u = User.objects.filter(login=login,password=password)[0]
+        all_users = User.objects.all()
+        u_json = list()
+        for k in all_users:
+            tmp = {"login":k.login,"longitude":k.longitude,"latitude":k.latitude}
+            u_json.append(tmp)
+        u_json = json.dumps(u_json)
+        #u_json = serializers.serialize('json', User.objects.all())
+    except:
+        u_json = 'Authentication failed'    
+    
+    return HttpResponse(u_json)
 
-    u = User.objects.filter(login=login,password=password)
-    return HttpResponse(u)
+def sendloc(request, login, password,longitude,latitude):
+    try:
+        u = User.objects.filter(login=login,password=password)[0]
+        u.longitude = longitude
+        u.latitude = latitude
+        u.save()
+        data = [longitude,latitude]
+    except:
+        data = 'Authentication failed'    
+    
+    return HttpResponse(data)
+
+def register(request,login,password)
+    pass
 
